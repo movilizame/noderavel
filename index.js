cee3e62259046dbaea0247bde1f7fd415d95309c
Mariano Botta <marianoabotta@gmail.com>
2018-04-03 18:45:44 -0300

    Redis client separate

--- index.js
+++ index.js
@@ -4,36 +4,34 @@ const EventEmitter = require('events');
 
 
 class Noderavel extends EventEmitter {
-    constructor ({ driver = 'redis', options = {}, scope = {}, queue = 'default', timeout = 60000 }) {
+    constructor ({ driver = 'redis', client, scope = {}, queue = 'default' }) {
         super();
         this.driver = driver;
-        this.options = options;
+        this.client = client;
         this.scope = scope;
         this.queue = queue;
-        this.timeout = timeout;
 
-        this.proccess();
-    }
-
-    connect () {
         switch (this.driver) {
-            case 'redis': 
-                const redis = require('redis');
-                const redisHost = this.options.host || '127.0.0.1';
-                const redisPort = this.options.port || '6379';
-                this.connection = redis.createClient(redisPort, redisHost); 
+            case 'redis':
+                this.redisPop();
                 break;
         }
     }
 
-    proccess () {
-        if (!this.connection) {
-            this.connect();
-        }
-    
+    // connect () {
+    //     switch (this.driver) {
+    //         case 'redis': 
+    //             const redis = require('redis');
+    //             const redisHost = this.options.host || '127.0.0.1';
+    //             const redisPort = this.options.port || '6379';
+    //             this.connection = redis.createClient(redisPort, redisHost); 
+    //             break;
+    //     }
+    // }
+
+    redisPop () { 
         const pop = () => {
-            this.connection.blpop('queues:' + this.queue, this.timeout, (err, replay) => {
-                console.log('alog');
+            this.client.blpop('queues:' + this.queue, 60000, (err, replay) => {
                 if (err) {
                     console.log('ERROR', err); 
                 } else { 
@@ -45,7 +43,6 @@ class Noderavel extends EventEmitter {
                 pop();
             }); 
         };
-    
         pop();
     }
 
--- test.js
+++ test.js
@@ -1,19 +1,17 @@
 const Noderavel = require ('./index.js');
 
+const redis = require('redis');
+const redisHost = '172.17.0.5';
+const redisPort = 6379;
+redisCliente = redis.createClient(redisPort, redisHost); 
 
 let queueWorker = new Noderavel({
-    options: {
-        host: '172.17.0.5',
-        port: 6379
-    },
+    client: redisCliente,
+    driver: 'redis',
     scope: {
         'STS\\Jobs\\TestJob': class App {}
     }
 });
-
-// queueWorker.onJob((c, obj) => {
-//     console.log(c, obj);
-// });
 
 queueWorker.on('job', ({name, data}) => {
     console.log(name, data);
